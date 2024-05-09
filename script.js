fabb2d2517d6704021eec1f2c963fb0fa5c933a7
Ninoguess <nino.samushia.90@gmail.com>
2024-05-10 01:23:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const sectionArray = [...document.getElementsByClassName("section")];
+
+let openSection = null;
+
+sectionArray.forEach((section) => {
+    section.addEventListener("click", () => {
+        if (section == openSection) {
+            section.nextElementSibling.computedStyleMap.display = "none";
+            openSection = null;
+        } else {
+            if (openSection != null) {
+                openSection.nextElementSibling.style.display = "none";
+            }
+            section.nextElementSibling.style.display = "block";
+            openSection = section;
+        }
+    });
+});
